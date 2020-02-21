84747296bddd6a433262985f9cf194e516863a22
mpaolodr <mpaolodr_1504@yahoo.com>
2020-02-21 12:24:58 -0500

    getTasks() and addTask() created and used in endpoints to add and get tasks

--- routers/projects/projects-model.js
+++ routers/projects/projects-model.js
@@ -4,9 +4,28 @@ function get() {
   return db("projects");
 }
 
+function getTasks(id) {
+  return db("projects")
+    .where({ id })
+    .then(found => {
+      if (found.length !== 0) {
+        return db("tasks as t")
+          .join("projects as p", "t.project_id", "p.id")
+          .select(
+            "p.name",
+            "p.description",
+            "t.description as task",
+            "t.notes",
+            "t.completed"
+          )
+          .where("t.project_id", id);
+      }
+    });
+}
+
 function add(data) {
   return db("projects")
-    .insert(data)
+    .insert(data, "id")
     .then(id => {
       return db("projects")
         .where({ id: id[0] })
@@ -14,7 +33,24 @@ function add(data) {
     });
 }
 
+function addTask(data, id) {
+  return db("projects")
+    .where({ id })
+    .first()
+    .then(project => {
+      return db("tasks")
+        .insert(data, "id")
+        .then(added => {
+          return db("tasks")
+            .where({ id: added[0] })
+            .first();
+        });
+    });
+}
+
 module.exports = {
   get,
-  add
+  getTasks,
+  add,
+  addTask
 };
--- routers/projects/projectsRouter.js
+++ routers/projects/projectsRouter.js
@@ -11,6 +11,22 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id/tasks", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const tasks = await Proj.getTasks(id);
+
+    if (tasks) {
+      res.status(200).json(tasks);
+    } else {
+      res.status(400).json({ errorMessage: "Invalid porject ID" });
+    }
+  } catch (err) {
+    res.status(500).json({ errorMessage: err.message });
+  }
+});
+
 router.post("/", async (req, res) => {
   const projData = req.body;
 
@@ -23,6 +39,22 @@ router.post("/", async (req, res) => {
     }
   } else {
     res.status(400).json({ errorMessage: "Missing name field" });
+  }
+});
+
+router.post("/:id/tasks", async (req, res) => {
+  const { id } = req.params;
+  const taskData = { ...req.body, project_id: id };
+
+  if (taskData.description) {
+    try {
+      const task = await Proj.addTask(taskData, id);
+      res.status(201).json(task);
+    } catch (err) {
+      res.status(500).json({ errorMessage: err.message });
+    }
+  } else {
+    res.status(400).json({ errorMessage: "Missing description field" });
   }
 });
 
